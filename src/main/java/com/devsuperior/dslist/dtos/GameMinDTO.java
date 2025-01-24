018561ae5d5f73de29d336e883e3b2c744ed8574
Murilo Polli <murilopolli@gmail.com>
2025-01-23 21:30:23 -0300

    get games by list

--- src/main/java/com/devsuperior/dslist/dtos/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dtos/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dslist.dtos;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -14,12 +15,19 @@ public class GameMinDTO {
 	}
 	
 	public GameMinDTO(Game entity) {
-		super();
 		this.id = entity.getId();
 		this.title = entity.getTitle();
 		this.year = entity.getYear();
 		this.imgUrl = entity.getImgUrl();
 		this.shortDescription = entity.getShortDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		this.id = projection.getId();
+		this.title = projection.getTitle();
+		this.year = projection.getYear();
+		this.imgUrl = projection.getImgUrl();
+		this.shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dslist.dtos.GameDTO;
 import com.devsuperior.dslist.dtos.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repsitories.GameRepository;
 
 @Service
@@ -28,4 +29,12 @@ public class GameService {
 		Game game = gameRepository.findById(id).get();
 		return new GameDTO(game);
 	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> games = gameRepository.searchByList(listId);
+		return games.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	
 }
